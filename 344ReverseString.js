be18cd525a4f8f4f124d06cb51adb480283aa9ae
PranaV Shimpi <23pranavshimpi@gmail.com>
2025-02-03 10:47:53 +0530

    Create 344ReverseString.js

---
+++ 344ReverseString.js
@@ -0,0 +1,16 @@
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while(left < right){
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+};
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
