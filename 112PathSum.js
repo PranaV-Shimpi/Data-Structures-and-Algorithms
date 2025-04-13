fe140b0f6071032317ec2caf7d20c7a275fc1115
PranaV Shimpi <23pranavshimpi@gmail.com>
2025-04-13 22:07:48 +0530

    Create 112PathSum.js

---
+++ 112PathSum.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+
+    if (!root.left && !root.right) {
+        return targetSum - root.val === 0;
+    }
+
+    targetSum -= root.val;
+
+    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);    
+};
+
+// Example 1:
+
+
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+// Output: true
+// Explanation: The root-to-leaf path with the target sum is shown.
+// Example 2:
+
+
+// Input: root = [1,2,3], targetSum = 5
+// Output: false
+// Explanation: There are two root-to-leaf paths in the tree:
+// (1 --> 2): The sum is 3.
+// (1 --> 3): The sum is 4.
+// There is no root-to-leaf path with sum = 5.
+// Example 3:
+
+// Input: root = [], targetSum = 0
+// Output: false
+// Explanation: Since the tree is empty, there are no root-to-leaf paths.
