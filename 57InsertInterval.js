111f06f5adb3b387910b01b8c5b508ee27ac236f
PranaV Shimpi <23pranavshimpi@gmail.com>
2025-05-19 11:37:03 +0530

    Create 57InsertInterval.js

---
+++ 57InsertInterval.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (intervals, newInterval) {
+    const result = [];
+    let i = 0;
+    const n = intervals.length;
+
+    // Add intervals before newInterval
+    while (i < n && intervals[i][1] < newInterval[0]) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    // Merge overlapping intervals
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+
+    result.push(newInterval);
+
+    // Add the rest
+    while (i < n) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    return result;
+  
+};
+
+// Example 1:
+
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+// Example 2:
+
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ 
