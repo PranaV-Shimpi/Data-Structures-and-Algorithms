1ac445df8057cddf2173d6be1bcaeb7985f751e7
PranaV Shimpi <23pranavshimpi@gmail.com>
2025-07-29 23:02:41 +0530

    Create 83RemoveDuplicatesFromSortedList.js

---
+++ 83RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if (head === null) return null;
+    
+    let temp = head;
+
+    while(head && head.next){
+        if(head.val === head.next.val){
+            head.next = head.next.next
+        }else{
+            head = head.next
+        }
+    }
+
+    return temp;
+};
+
+
+// Example 1:
+
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+// Example 2:
+
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
