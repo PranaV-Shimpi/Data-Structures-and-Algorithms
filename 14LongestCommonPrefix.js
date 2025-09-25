597d81b301a0d7d188884e59298a039e63bb3ea6
PranaV Shimpi <23pranavshimpi@gmail.com>
2025-09-26 00:01:29 +0530

    Create 14LongestCommonPrefix.js

---
+++ 14LongestCommonPrefix.js
@@ -0,0 +1,33 @@
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let pref = strs[0];
+    let prefLen = pref.length;
+
+    for (let i = 1; i < strs.length; i++) {
+        let s = strs[i];
+        while (pref !== s.substring(0, prefLen)) {
+            prefLen--;
+            if (prefLen === 0) {
+                return "";
+            }
+            pref = pref.substring(0, prefLen);
+        }
+    }
+
+    return pref;
+};
+
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
